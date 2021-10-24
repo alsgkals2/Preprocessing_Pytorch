38b68de25c3eee585c3ad562243fcffaf1bbab66
DESKTOP-KP97HUM\minha <kimminha1994@gmail.com>
2021-10-25 02:49:40 +0900

    Ensemble with data crop

---
+++ Ensemble/Common_Function_.py
@@ -0,0 +1,16 @@
+import torch
+import numpy as np
+
+import os
+import random
+
+def set_seeds(seed=42):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False  # for faster training, but not deterministic
+

0276794044fd1d4cf137a40632640e20762676da
Minha Kim <46020887+alsgkals2@users.noreply.github.com>
2024-10-22 13:03:03 +0900

    [Feature]test_onnxfile_using_python
    [Feature]test_onnxfile_using_python

---
+++ onnx_tflite/onnx_infer_example_threshold.py
@@ -0,0 +1,33 @@
+import onnxruntime as ort
+import numpy as np
+import cv2
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+    
+onnx_model_path = 'filename.onnx'
+thres = 0.9 # hyper param
+
+#input test model
+file_name = 'testimage.jpb'
+#load img
+session = ort.InferenceSession(onnx_model_path)
+img = cv2.imread(file_name)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = cv2.resize(img, (92,92))
+img = (img/255.).astype(np.float32)
+img = img.transpose((2,0,1))
+
+#transform img
+mean = np.array([0.485, 0.456, 0.406])
+std = np.array([0.229, 0.224, 0.225])
+img[0] = (img[0] - mean[0]) / std[0] 
+img[1] = (img[1] - mean[1]) / std[1] 
+img[2] = (img[2] - mean[2]) / std[2] 
+
+output_names = [session.get_outputs()[0].name]
+input_name = session.get_inputs()[0].name
+outputs = session.run(output_names, {input_name: img[np.newaxis,:]})
+outputs = sigmoid(outputs[0])
+print("Output:", outputs[0]) 
+print(f"Trehosld:{thres} -->", 'Occlusion' if outputs[0]>thres else 'Normal')  
